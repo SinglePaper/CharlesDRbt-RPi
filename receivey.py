d9dbd02d4aa6d4c3792e1105e3ced4d0a0263bd9
SinglePaper <stefanhasenoot@hotmail.com>
2021-12-19 13:29:11 +0100

    Update

--- receivey.py
+++ receivey.py
@@ -1,71 +1,5 @@
-### Raspberry Pi code (Server)
-
-
-## Using Wifi connection
-# import socket
-# import RPi.GPIO as GPIO
-# import time
-
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setup(16,GPIO.OUT)  # Left motor forward
-# GPIO.setup(11,GPIO.OUT)  # Left motor backward
-# GPIO.setup(13,GPIO.OUT)  # Right motor forward
-# GPIO.setup(15,GPIO.OUT)  # Right motor backward
-
-
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.bind(("0.0.0.0", 5432))
-# s.listen(5)
-
-# while True:
-#     try:
-#         print("Searching for a connection...")
-#         clientsocket, address = s.accept()
-#         print(f"Successful connection with address {address[0]}.")
-#         while True:
-#             data = clientsocket.recv(1).decode("utf-8")
-#             if not data:
-#                 break
-#             print(f"{address[0]}: " + data)
-#             if data == "f":
-#                 GPIO.output(16, False)
-#                 GPIO.output(11, True)
-#                 GPIO.output(13, False)
-#                 GPIO.output(15, True)
-#             elif data == "b":
-#                 GPIO.output(16, True)
-#                 GPIO.output(11, False)
-#                 GPIO.output(13, True)
-#                 GPIO.output(15, False)
-#             elif data == "l":
-#                 GPIO.output(16, False)
-#                 GPIO.output(11, False)
-#                 GPIO.output(13, False)
-#                 GPIO.output(15, True)
-#             elif data == "r":
-#                 GPIO.output(16, False)
-#                 GPIO.output(11, True)
-#                 GPIO.output(13, False)
-#                 GPIO.output(15, False)
-#             else:
-#                 GPIO.output(16, False)
-#                 GPIO.output(11, False)
-#                 GPIO.output(13, False)
-#                 GPIO.output(15, False)
-#         else:
-#             continue
-#     except:
-#         GPIO.cleanup()
-#         s.close()
-#         if 'clientsocket' in locals():
-#             clientsocket.close()
-#         break
-
-
-
-## Using JS Website connection
-from flask import Flask, jsonify, request, render_template, Response
-import random, json
+from flask import Flask, request, render_template
+import json
 import logging
 from os import system
 
@@ -98,6 +32,8 @@ speed = 0
 #    0
 #  1 2 3
 ##
+
+# Each detection should only move the robot slightly. This resets the outputs after .1 seconds and is called if AI is True in the input
 def reset_after_AI():
     global speed
     global direction
@@ -122,9 +58,9 @@ def receiver():
     # Extract the data from data into global variables to use to control the motor
     if type(data) == str:
         data = json.loads(data)
-    direction = data['direction']
-    speed = data['speed']
-    AI = data['AI']
+    direction = data['direction']   # Direction
+    speed = data['speed']           # Turn on motors?
+    AI = data['AI']                 # Does this command come from the AI?
 
     if direction == 0:  # Forward
         GPIO.output(16, True)
@@ -152,14 +88,15 @@ def receiver():
         GPIO.output(13, False)
         GPIO.output(15, False)
 
-    if speed == 0:        # Can't happen yet, but just to make sure I remember what to do with this variable. Speed 0 = still, Speed 1 = normal, Speed 2 = fast
+    if speed == 0:        # Speed 0 = still, Speed 1 = normal
         GPIO.output(16, False)
         GPIO.output(11, False)
         GPIO.output(13, False)
         GPIO.output(15, False)
     
-    # Speed can be implemented by repeatedly turning the motors on and off on given intervals, but can't be bothered to do that right now.
+    # To prevent large movements as a result of AI detection
     if AI: reset_after_AI()
+
     # Print out some debugging tools and make it look pretty :)
     system('clear')
     print("\n=======Controls=======")
